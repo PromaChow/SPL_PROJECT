456139aa87e6f9f29c61254a78d1b748575ae7f5
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-01-27 23:13:27 +0600

    Add files via upload

--- LearnToAnalyzePcap.c
+++ LearnToAnalyzePcap.c
@@ -14,10 +14,10 @@ struct GlobalHeader
 
 struct PacketHeader
 {
-    unsigned int tSec;  /* timestamp seconds */
-    unsigned int tuSec; /* timestamp microseconds */
-    unsigned int ocLen; /* number of octets of packet saved in file */
-    unsigned int packLen;  //actual length of packet
+    unsigned int tSec;  
+    unsigned int tuSec; 
+    unsigned int ocLen; 
+    unsigned int packLen;  
 };
 
 struct EthernetHeader
@@ -28,9 +28,21 @@ struct EthernetHeader
 };
 
 struct IP
-{
+{   unsigned char IHL;
     unsigned char source[4];
     unsigned char destination[4];
+};
+struct TCP{
+    unsigned short int srcport;
+    unsigned short int destport;
+    unsigned int       seqNum;
+    unsigned int       ackNUm;
+    unsigned char      tcp_resoff;
+    unsigned char      tcp_flag;
+    unsigned short int tcp_win;
+    unsigned short int tcp_checksum;
+    unsigned short int tcp_urgptr;
+
 };
 
 
@@ -42,6 +54,7 @@ int main()
     struct PacketHeader phead;
     struct EthernetHeader ehead;
     struct IP ip;
+    struct TCP T;
     
     unsigned char c, protocol;
     unsigned short g;
@@ -80,9 +93,12 @@ int main()
     }
     printf("Ethernet type: %x\n",ehead.ethType);
 
-    fread(&c, sizeof(char), 1, pf); //taking IP Version Number;
-    c=c>>4;
-    printf("IP Version Number: %d\n",(int)c);
+    fread(&ip.IHL, sizeof(char), 1, pf); //taking IP Version Number;
+    unsigned short p,len;
+    p=ip.IHL >>4;
+    len=(ip.IHL & 0x0f);
+    printf("IP Version Number: %d\n",(int)p);
+    printf("IP header length is %d\n",(int)len*4);
 
     for(int i=0;i<8;i++)
         fread(&c, sizeof(char), 1, pf);
@@ -107,6 +123,47 @@ int main()
         if(i!=3) printf(".");
         else printf("\n");
     }
+    int i=0;
+    len=len*4;
+    len=len-20;
+    char ch;
+    while(i<len){
+        ch=fgetc(pf);
+
+    }
+   
+    //fread(&a,sizeof(unsigned int),1,pf);
+    fread(&T,sizeof(struct TCP),1,pf);
+    printf("SOURCE PORT: %d\n",ntohs(T.srcport));
+    printf("DESTINATION PORT : %d\n",ntohs(T.destport));
+    int URG,ACK,PSH,RST,SYN,FIN;
+    int bit;
+     bit=(int)T.tcp_flag>>7;
+    if(bit&1==1) URG=1; else URG=0; 
+    bit=(int)T.tcp_flag>>6;
+
+    if(bit&1==1)  ACK=1; else ACK=0; 
+    
+    bit=(int)T.tcp_flag>>5;
+    if(bit&1==1)  PSH=1; else PSH=0;
+ 
+     bit=(int)T.tcp_flag>>4;
+    if(bit&1==1)  RST=1; else RST=0;
+   
+     bit=(int)T.tcp_flag>>3;
+    if(bit&1==1) SYN=1; else SYN=0;
+    bit=(int)T.tcp_flag>>2;
+    if(bit&1==1) FIN=1; else FIN=0;
+
+    printf("FLAGS:\nURG : %d\nACK : %d\nPSH : %d\nRST : %d\nSYN : %d\nFIN : %d\n",URG,ACK,PSH,RST,SYN,FIN);
+   
+
+
+    
+
+
+    
+
 }
 
 
