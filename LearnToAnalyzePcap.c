daf3f37914cfe8592a9624e23a848abd8bedd6f3
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-01-31 09:39:46 +0600

    Add files via upload

--- LearnToAnalyzePcap.c
+++ LearnToAnalyzePcap.c
@@ -3,18 +3,18 @@
 
 struct GlobalHeader
 {
-    unsigned int magicNumber;
+    unsigned int       magicNumber;
     unsigned short int versionMajor;
     unsigned short int versionMinor;
-    int time;
-    unsigned int sigfigs;
-    unsigned int snaplen;
-    unsigned int network;
+    int                time;
+    unsigned int       sigfigs;
+    unsigned int       snaplen;
+    unsigned int       network;
 };
 
 struct PacketHeader
 {
-    unsigned int tSec;  
+    unsigned int tSec;
     unsigned int tuSec; 
     unsigned int ocLen; 
     unsigned int packLen;  
@@ -22,8 +22,8 @@ struct PacketHeader
 
 struct EthernetHeader
 {
-    unsigned char destination[6];
-    unsigned char source[6];
+    unsigned char      destination[6];
+    unsigned char      source[6];
     unsigned short int ethType;
 };
 
@@ -42,7 +42,6 @@ struct TCP{
     unsigned short int tcp_win;
     unsigned short int tcp_checksum;
     unsigned short int tcp_urgptr;
-
 };
 
 
@@ -58,8 +57,12 @@ int main()
     
     unsigned char c, protocol;
     unsigned short g;
+    char flg[6];
     
     FILE *pf= fopen("iit2.pcap","rb");
+
+    // for(int it=0; it<2; it++)
+    // {
 
     fread(&ghead, sizeof(struct GlobalHeader), 1, pf);
 
@@ -70,9 +73,17 @@ int main()
     printf("snaplen: %x\n",ghead.snaplen);
     printf("network: %x\n",ghead.network);
 
-    fread(&phead, sizeof(struct PacketHeader), 1, pf);
+    for(int it=0; !feof(pf); it++)
+    {
 
-    printf("length of packet: %d\n",phead.packLen);
+    printf("pack#%d:\n",it+1);
+     fread(&phead, sizeof(struct PacketHeader), 1, pf);
+    // for(int x=0;x<8;x++)
+    //     c=fgetc(pf);
+
+    // fread(&phead.packLen,sizeof(unsigned int), 1, pf);
+
+    printf("length of packet: %d\n",phead.ocLen);
 
     fread(&ehead, sizeof(struct EthernetHeader), 1, pf);
     ehead.ethType= ntohs(ehead.ethType);
@@ -99,7 +110,7 @@ int main()
     len=(ip.IHL & 0x0f);
     printf("IP Version Number: %d\n",(int)p);
     printf("IP header length is %d\n",(int)len*4);
-
+    //printf("%dhihi\n",len);
     for(int i=0;i<8;i++)
         fread(&c, sizeof(char), 1, pf);
     fread(&protocol, sizeof(char), 1, pf); //taking protocol
@@ -107,7 +118,7 @@ int main()
     fread(&ip.source, sizeof(unsigned char)*4, 1, pf);
     fread(&ip.destination, sizeof(unsigned char)*4, 1, pf);
 
-    if(protocol==6) printf("this is TCP\n");
+    if(protocol==6) printf("this is TCP protocol\n");
     else printf("this is not TCP\n");
     printf("source IP:");
     for(int i=0; i<4; i++)
@@ -127,45 +138,44 @@ int main()
     len=len*4;
     len=len-20;
     char ch;
-    while(i<len){
+    /*while(i<len){
         ch=fgetc(pf);
-
-    }
+        printf("here");
+    }*/
    
     //fread(&a,sizeof(unsigned int),1,pf);
     fread(&T,sizeof(struct TCP),1,pf);
     printf("SOURCE PORT: %d\n",ntohs(T.srcport));
     printf("DESTINATION PORT : %d\n",ntohs(T.destport));
-    int URG,ACK,PSH,RST,SYN,FIN;
-    int bit;
-     bit=(int)T.tcp_flag>>7;
-    if(bit&1==1) URG=1; else URG=0; 
-    bit=(int)T.tcp_flag>>6;
-
-    if(bit&1==1)  ACK=1; else ACK=0; 
     
-    bit=(int)T.tcp_flag>>5;
-    if(bit&1==1)  PSH=1; else PSH=0;
- 
-     bit=(int)T.tcp_flag>>4;
-    if(bit&1==1)  RST=1; else RST=0;
-   
-     bit=(int)T.tcp_flag>>3;
-    if(bit&1==1) SYN=1; else SYN=0;
-    bit=(int)T.tcp_flag>>2;
-    if(bit&1==1) FIN=1; else FIN=0;
-
-    printf("FLAGS:\nURG : %d\nACK : %d\nPSH : %d\nRST : %d\nSYN : %d\nFIN : %d\n",URG,ACK,PSH,RST,SYN,FIN);
-   
+    int j= 0;
+    //T.tcp_flag=T.tcp_flag>>2;
+    for(int i=32;i>=1;i=i>>1)
+    {
+        if(T.tcp_flag & i) flg[j++]= 1;
+        else flg[j++]= 0;
+    }
 
 
-    
 
 
-    
+    // for(int i=0;i<6;i++)
+    //     printf("%d\t",flg[i]);
+
+    printf("URG : %d\n",flg[0]);
+    printf("ACK : %d\n",flg[1]);
+    printf("PSH : %d\n",flg[2]);
+    printf("RST : %d\n",flg[3]);
+    printf("SYN : %d\n",flg[4]);
+    printf("FIN : %d\n",flg[5]);
+    //printf("WINDOW SIZE \n%d\n",ntohs(T.tcp_win));
+
+    for(int bb=0; bb<phead.ocLen-54;bb++)
+        c=fgetc(pf);
+
+    }
 
 }
-
 
 
 
