a3c5910555f97961e2affa42df85c69ac296b80c
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-03-11 21:01:55 +0600

    Add files via upload

--- LearnToAnalyzePcap.c
+++ LearnToAnalyzePcap.c
@@ -15,9 +15,9 @@ struct GlobalHeader
 struct PacketHeader
 {
     unsigned int tSec;
-    unsigned int tuSec; 
-    unsigned int ocLen; 
-    unsigned int packLen;  
+    unsigned int tuSec;
+    unsigned int ocLen;
+    unsigned int packLen;
 };
 
 struct EthernetHeader
@@ -28,11 +28,13 @@ struct EthernetHeader
 };
 
 struct IP
-{   unsigned char IHL;
+{
+    unsigned char IHL;
     unsigned char source[4];
     unsigned char destination[4];
 };
-struct TCP{
+struct TCP
+{
     unsigned short int srcport;
     unsigned short int destport;
     unsigned int       seqNum;
@@ -44,7 +46,13 @@ struct TCP{
     unsigned short int tcp_urgptr;
 };
 
-
+struct UDP
+{
+    unsigned short int  srcport;
+    unsigned short int  destport;
+    unsigned short      len;
+    unsigned short      udp_checksum;
+};
 
 
 int main()
@@ -54,15 +62,15 @@ int main()
     struct EthernetHeader ehead;
     struct IP ip;
     struct TCP T;
+    struct UDP U;
     
+
     unsigned char c, protocol;
     unsigned short g;
     char flg[6];
-    
+
     FILE *pf= fopen("iit2.pcap","rb");
 
-    // for(int it=0; it<2; it++)
-    // {
 
     fread(&ghead, sizeof(struct GlobalHeader), 1, pf);
 
@@ -76,106 +84,143 @@ int main()
     for(int it=0; !feof(pf); it++)
     {
 
-    printf("pack#%d:\n",it+1);
-     fread(&phead, sizeof(struct PacketHeader), 1, pf);
-    // for(int x=0;x<8;x++)
-    //     c=fgetc(pf);
+        printf("\n%c[4mpack#%d:\n%c[0m",27, it+1, 27);
+        fread(&phead, sizeof(struct PacketHeader), 1, pf);
 
-    // fread(&phead.packLen,sizeof(unsigned int), 1, pf);
+        printf("length of packet: %d\n",phead.ocLen);
 
-    printf("length of packet: %d\n",phead.ocLen);
+        fread(&ehead, sizeof(struct EthernetHeader), 1, pf);
+        ehead.ethType= ntohs(ehead.ethType);
 
-    fread(&ehead, sizeof(struct EthernetHeader), 1, pf);
-    ehead.ethType= ntohs(ehead.ethType);
+        printf("Destination Address:");
+        for(int i=0; i<6; i++)
+        {
+            printf("%02x",ehead.destination[i]);
+            if(i!=5)printf(":");
+            else printf("\n");
+        }
+        printf("Source Address:");
+        for(int i=0; i<6; i++)
+        {
+            printf("%02x",ehead.source[i]);
+            if(i!=5)printf(":");
+            else printf("\n");
+        }
+        printf("Ethernet type: %x\n",ehead.ethType);
 
-    printf("Destination Address:");
-    for(int i=0; i<6; i++)
-    {
-        printf("%02x",ehead.destination[i]);
-        if(i!=5)printf(":");
-        else printf("\n");
-    }
-    printf("Source Address:");
-    for(int i=0; i<6; i++)
-    {
-        printf("%02x",ehead.source[i]);
-        if(i!=5)printf(":");
-        else printf("\n");
-    }
-    printf("Ethernet type: %x\n",ehead.ethType);
+        fread(&ip.IHL, sizeof(char), 1, pf); //taking IP Version Number;
+        unsigned short p,len;
+        p=ip.IHL >>4;
+        len=(ip.IHL & 0x0f);
+        printf("IP Version Number: %d\n",(int)p);
+        printf("IP header length is %d\n",(int)len*4);
 
-    fread(&ip.IHL, sizeof(char), 1, pf); //taking IP Version Number;
-    unsigned short p,len;
-    p=ip.IHL >>4;
-    len=(ip.IHL & 0x0f);
-    printf("IP Version Number: %d\n",(int)p);
-    printf("IP header length is %d\n",(int)len*4);
-    //printf("%dhihi\n",len);
-    for(int i=0;i<8;i++)
-        fread(&c, sizeof(char), 1, pf);
-    fread(&protocol, sizeof(char), 1, pf); //taking protocol
-    fread(&g, sizeof(unsigned short), 1, pf);
-    fread(&ip.source, sizeof(unsigned char)*4, 1, pf);
-    fread(&ip.destination, sizeof(unsigned char)*4, 1, pf);
+        for(int i=0; i<8; i++)
+            fread(&c, sizeof(char), 1, pf);
+        fread(&protocol, sizeof(char), 1, pf); //taking protocol
+        fread(&g, sizeof(unsigned short), 1, pf);
+        fread(&ip.source, sizeof(unsigned char)*4, 1, pf);
+        fread(&ip.destination, sizeof(unsigned char)*4, 1, pf); //54 bytes read from packet
 
-    if(protocol==6) printf("this is TCP protocol\n");
-    else printf("this is not TCP\n");
-    printf("source IP:");
-    for(int i=0; i<4; i++)
-    {
-        printf("%d",(int)ip.source[i]);
-        if(i!=3) printf(".");
-        else printf("\n");
-    }
-    printf("destination IP:");
-    for(int i=0; i<4; i++)
-    {
-        printf("%d",(int)ip.destination[i]);
-        if(i!=3) printf(".");
-        else printf("\n");
-    }
-    int i=0;
-    len=len*4;
-    len=len-20;
-    char ch;
-    /*while(i<len){
-        ch=fgetc(pf);
-        printf("here");
-    }*/
-   
-    //fread(&a,sizeof(unsigned int),1,pf);
-    fread(&T,sizeof(struct TCP),1,pf);
-    printf("SOURCE PORT: %d\n",ntohs(T.srcport));
-    printf("DESTINATION PORT : %d\n",ntohs(T.destport));
-    
-    int j= 0;
-    //T.tcp_flag=T.tcp_flag>>2;
-    for(int i=32;i>=1;i=i>>1)
-    {
-        if(T.tcp_flag & i) flg[j++]= 1;
-        else flg[j++]= 0;
-    }
+        printf("source IP:");
+        for(int i=0; i<4; i++)
+        {
+            printf("%d",(int)ip.source[i]);
+            if(i!=3) printf(".");
+            else printf("\n");
+        }
+        printf("destination IP:");
+        for(int i=0; i<4; i++)
+        {
+            printf("%d",(int)ip.destination[i]);
+            if(i!=3) printf(".");
+            else printf("\n");
+        }
+        int i=0;
+        len=len*4;
+        len=len-20;
+        char ch;
+        if(protocol==6)
+        {
+            printf("this is TCP protocol\n");
+            fread(&T,sizeof(struct TCP),1,pf);
+            printf("SOURCE PORT: %d\n",ntohs(T.srcport));
+            printf("DESTINATION PORT : %d\n",ntohs(T.destport));
+
+            int j= 0;
+            //T.tcp_flag=T.tcp_flag>>2;
+            for(int i=32; i>=1; i=i>>1)
+            {
+                if(T.tcp_flag & i) flg[j++]= 1;
+                else flg[j++]= 0;
+            }
+            for(int i=0; i<6; i++)
+                {
+                    if(i==0)
+                printf("URG : %d\n",flg[i]);
+
+                 if(i==1)
+                printf("ACK: %d\n",flg[i]);
+
+                 if(i==2)
+                printf("PSH : %d\n",flg[i]);
+
+                 if(i==3)
+                printf("RST : %d\n",flg[i]);
+
+                 if(i==4)
+                printf("SYN : %d\n",flg[i]);
+
+                 if(i==5)
+                printf("FIN : %d\n",flg[i]);
 
 
+            //printf("\n%d\n",ntohs(T.tcp_win));
+        }
+        }
 
+        else if(protocol==17)
+        {
+            printf("this is UDP protocol\n");
+            fread(&U, sizeof(struct UDP), 1, pf);
+            printf("SOURCE PORT: %d\n",ntohs(U.srcport));
+            printf("DESTINATION PORT : %d\n",ntohs(U.destport));
+           // printf("UDP length: %d\n ",ntohs(U.len));
 
-    // for(int i=0;i<6;i++)
-    //     printf("%d\t",flg[i]);
+            for(int i=0; i<12; i++)
+                c= fgetc(pf);
+        }
 
-    printf("URG : %d\n",flg[0]);
-    printf("ACK : %d\n",flg[1]);
-    printf("PSH : %d\n",flg[2]);
-    printf("RST : %d\n",flg[3]);
-    printf("SYN : %d\n",flg[4]);
-    printf("FIN : %d\n",flg[5]);
-    //printf("WINDOW SIZE \n%d\n",ntohs(T.tcp_win));
+        else
+        {
+            if(protocol==0)printf("This is HOPOPT protocol");
+            else if(protocol==1)printf("This is ICMP protocol");
+            else if(protocol==2)printf("This is IGMP protocol");
+            else if(protocol==3)printf("This is GGP protocol");
+            else if(protocol==4)printf("This is IP-in-IP protocol");
+            else if(protocol==5)printf("This is ST protocol");
+            else if(protocol==7)printf("This is CBT protocol");
+            else if(protocol==8)printf("This is EGP protocol");
+            else if(protocol==9)printf("This is IGP protocol");
+            else if(protocol==10)printf("This is BBN-RCC-MON protocol");
+            else if(protocol==11)printf("This is NVP-II protocol");
+            else if(protocol==12)printf("This is PUP protocol");
+            else if(protocol==13)printf("This is ARGUS protocol");
+            else if(protocol==14)printf("This is EMCON protocol");
+            else if(protocol==15)printf("This is XNET protocol");
+            else if(protocol==16)printf("This is CHAOS protocol");
+            else if(protocol==18)printf("This is MUX protocol");
+            else if(protocol==19)printf("This is DCN-MEANS protocol");
+            else if(protocol==20)printf("This is HMP protocol");
+            else printf("protocol number: %d\n",protocol);
+            for(int i=0; i<20; i++)
+                c= fgetc(pf);
+        }
+            
 
-    for(int bb=0; bb<phead.ocLen-54;bb++)
-        c=fgetc(pf);
+        for(int bb=0; bb<phead.ocLen-54; bb++)
+            c=fgetc(pf);
 
     }
 
 }
-
-
-
