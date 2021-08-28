d7712f17c1292bacc4ee9a9366cbd4d13ca54b2f
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2021-08-28 12:30:26 +0600

    Add files via upload

--- ping.c
+++ ping.c
@@ -22,136 +22,136 @@ struct timespec start, start1, end, end1;
 
 unsigned short chsum(unsigned short *ptr, int size)
 {
-    unsigned long int sum;
-    unsigned short oddbyte;
-    unsigned short int answer;
+	unsigned long int sum;
+	unsigned short oddbyte;
+	unsigned short int answer;
 
-    sum = 0;
-    while (size > 1)
-    {
-        sum += *ptr++;
-        size -= 2;
-    }
-    if (size == 1)
-    {
-        oddbyte = 0;
-        *((unsigned char *)&oddbyte) = *(unsigned char *)ptr;
-        sum += oddbyte;
-    }
+	sum = 0;
+	while (size > 1)
+	{
+		sum += *ptr++;
+		size -= 2;
+	}
+	if (size == 1)
+	{
+		oddbyte = 0;
+		*((unsigned char *)&oddbyte) = *(unsigned char *)ptr;
+		sum += oddbyte;
+	}
 
-    sum = (sum >> 16) + (sum & 0x0000ffff);
-    sum = sum + (sum >> 16);
-    answer = (short)~sum;
+	sum = (sum >> 16) + (sum & 0x0000ffff);
+	sum = sum + (sum >> 16);
+	answer = (short)~sum;
 
-    return answer;
+	return answer;
 }
 
 void stop_run(int l)
 {
-    run = 0;
-    sleep(1);
+	run = 0;
+	sleep(0.5);
 }
 
 void set_run(long long u)
 {
-    run = u;
+	run = u;
 }
 
 int sendPing(int u, char argv[])
 {
 
-    if (u != -1)
-        set_run(u);
+	if (u != -1)
+		set_run(u);
 
-    int s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-    if (s == -1)
-    {
-        perror("Failed to create socket");
-        exit(1);
-    }
-    double time, packetLoss;
-    char datagram[64], source_ip[16], desta[16], host_name[100];
-    memset(datagram, 0, 64);
+	int s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+	if (s == -1)
+	{
+		perror("Failed to create socket");
+		exit(1);
+	}
+	double time, packetLoss;
+	char datagram[64], source_ip[16], desta[16], host_name[100];
+	memset(datagram, 0, 64);
 
-    int TTL_def = 51;
+	int TTL_def = 64;
 
-    if (setsockopt(s, SOL_IP, IP_TTL, &TTL_def, sizeof(TTL_def)) != 0)
-    {
+	if (setsockopt(s, SOL_IP, IP_TTL, &TTL_def, sizeof(TTL_def)) != 0)
+	{
 
-        printf("Error setting TTL value");
-        return 0;
-    }
-    struct sockaddr_in destAddr;
+		printf("Error setting TTL value");
+		return 0;
+	}
+	struct sockaddr_in destAddr;
 
-    if (is_Host(argv))
-    {
-        convertHosttoIp(argv, desta, &destAddr);
-        strcpy(host_name, argv);
-    }
+	if (is_Host(argv))
+	{
+		convertHosttoIp(argv, desta, &destAddr);
+		strcpy(host_name, argv);
+	}
 
-    else
-    {
-        strcpy(desta, argv);
-        IPtoHostName(desta, host_name);
+	else
+	{
+		strcpy(desta, argv);
+		IPtoHostName(desta, host_name);
 
-        destAddr.sin_family = AF_INET;
-        destAddr.sin_port = htons(0);
-        destAddr.sin_addr.s_addr = inet_addr(desta);
-    }
+		destAddr.sin_family = AF_INET;
+		destAddr.sin_port = htons(0);
+		destAddr.sin_addr.s_addr = inet_addr(desta);
+	}
 
-    clock_gettime(CLOCK_MONOTONIC, &start);
+	clock_gettime(CLOCK_MONOTONIC, &start);
 
-    signal(SIGINT, stop_run);
-    c = 0, pack_rcv = 0;
-    //printf("%d\n",u);
+	signal(SIGINT, stop_run);
+	c = 0, pack_rcv = 0;
+	//printf("%d\n",u);
 
-    while (run--)
-    {
-        struct icmphdr *icmph = (struct icmphdr *)(datagram);
-        icmph->type = ICMP_ECHO;
-        icmph->code = 0;
-        icmph->checksum = chsum((unsigned short *)datagram, sizeof(datagram));
-        icmph->un.echo.id = getpid();
-        icmph->un.echo.sequence = c++;
+	while (run--)
+	{
+		struct icmphdr *icmph = (struct icmphdr *)(datagram);
+		icmph->type = ICMP_ECHO;
+		icmph->code = 0;
+		icmph->checksum = chsum((unsigned short *)datagram, sizeof(datagram));
+		icmph->un.echo.id = getpid();
+		icmph->un.echo.sequence = c++;
 
-        clock_gettime(CLOCK_MONOTONIC, &start1);
+		clock_gettime(CLOCK_MONOTONIC, &start1);
 
-        if (sendto(s, datagram, sizeof(datagram), 0, (struct sockaddr *)&destAddr, sizeof(destAddr)) <= 0)
-        {
-            printf("Sending failed\n");
-        }
+		if (sendto(s, datagram, sizeof(datagram), 0, (struct sockaddr *)&destAddr, sizeof(destAddr)) <= 0)
+		{
+			printf("Sending failed\n");
+		}
 
-        int rcv = recv(s, datagram, 64, 0);
-        if (rcv < 0)
-        {
-            printf("error in reading recv function\n");
+		int rcv = recv(s, datagram, 64, 0);
+		if (rcv < 0)
+		{
+			printf("error in reading recv function\n");
 
-            return -1;
-        }
+			return -1;
+		}
 
-        struct icmphdr *icmp = (struct icmphdr *)(datagram);
+		struct icmphdr *icmp = (struct icmphdr *)(datagram);
 
-        if (icmp->code != 0 && icmp->type != 69)
-        {
-            printf("ERROR PACKET RECEIVED WITH ICMP CODE %d AND ICMP TYPE %d\n", icmp->code, icmp->type);
-        }
-        else
-        {
-            clock_gettime(CLOCK_MONOTONIC, &end1);
-            time = ((end1.tv_nsec - start1.tv_nsec) / 1000000) + ((end1.tv_sec - start1.tv_sec) * 1000);
-            print(Cyan);
-            printf("64 bytes sent from %s(%s) icmp_seq=%d ttl=51 time=%f ms\n", desta, host_name, c, time);
-            refresh();
-            pack_rcv++;
-        }
-    }
+		if (icmp->code != 0 && icmp->type != 69)
+		{
+			printf("ERROR PACKET RECEIVED WITH ICMP CODE %d AND ICMP TYPE %d\n", icmp->code, icmp->type);
+		}
+		else
+		{
+			clock_gettime(CLOCK_MONOTONIC, &end1);
+			time = ((end1.tv_nsec - start1.tv_nsec) / 1000000) + ((end1.tv_sec - start1.tv_sec) * 1000);
+			print(Cyan);
+			printf("64 bytes sent from %s(%s) icmp_seq=%d ttl=64, time = %f ms\n", desta, host_name, c, time);
+			refresh();
+			pack_rcv++;
+		}
+	}
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
+	clock_gettime(CLOCK_MONOTONIC, &end);
 
-    time = ((end.tv_nsec - start.tv_nsec) / 1000000) + ((end.tv_sec - start.tv_sec) * 1000);
-    packetLoss = ((c - pack_rcv) * 100) / c;
-    char ch = '%';
+	time = ((end.tv_nsec - start.tv_nsec) / 1000000) + ((end.tv_sec - start.tv_sec) * 1000);
+	packetLoss = ((c - pack_rcv) * 100) / c;
+	char ch = '%';
 
-    printf("\n\n----------ping statistics---------\n");
-    printf("%d packets transmitted, %d received, %f%c packet loss, time=%f ms\n\n", c, pack_rcv, packetLoss, ch, time);
+	printf("\n\n----------ping statistics---------\n");
+	printf("%d packets transmitted, %d received, %f%c packet loss, time = %f ms\n\n", c, pack_rcv, packetLoss, ch, time);
 }
