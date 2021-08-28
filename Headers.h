ccfde9b720587f0bd2eeea9bfb299b1a2b1be9ca
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2021-08-28 12:20:27 +0600

    Add files via upload

--- Headers.h
+++ Headers.h
@@ -1,81 +1,79 @@
 #ifndef HEADERS_INCLUDED
 #define HEADERS_INCLUDED
+#include <linux/types.h>
 
-
-struct iphdr {
-	u_int32_t ihl:4,version:4;	
-	u_int8_t  tos;		
-	u_int16_t tot_len;		
-	u_int16_t id;		
-	u_int16_t frag_off;		
-	u_int8_t  ttl;		
-	u_int8_t  protocol;			
-	u_int16_t check;		
-	u_int32_t saddr;
+struct iphdr
+{
+    u_int32_t ihl : 4, version : 4;
+    u_int8_t tos;
+    u_int16_t tot_len;
+    u_int16_t id;
+    u_int16_t frag_off;
+    u_int8_t ttl;
+    u_int8_t protocol;
+    u_int16_t check;
+    u_int32_t saddr;
     u_int32_t daddr;
 };
 
-struct tcphdr{
+struct tcphdr
+{
     u_int16_t source;
     u_int16_t dest;
     u_int32_t seq;
     u_int32_t ack_seq;
-    u_int16_t skip:4;
-    u_int16_t doff:4;
-    u_int16_t fin:1;
-    u_int16_t syn:1;
-    u_int16_t rst:1;
-    u_int16_t psh:1;
-    u_int16_t ack:1;
-    u_int16_t urg:1;
-    u_int16_t skip2:2;
+    u_int16_t skip : 4;
+    u_int16_t doff : 4;
+    u_int16_t fin : 1;
+    u_int16_t syn : 1;
+    u_int16_t rst : 1;
+    u_int16_t psh : 1;
+    u_int16_t ack : 1;
+    u_int16_t urg : 1;
+    u_int16_t skip2 : 2;
     u_int16_t window;
     u_int16_t check;
     u_int16_t urg_ptr;
 };
 
-
-struct Echo{
-    u_int16_t        id;
-    u_int16_t        sequence;
-};
-struct Fragment{
-      u_int16_t        unused;
-      u_int16_t        mtu;
-    
-};
-
-union unanymoous{
-    struct Echo echo;
-    struct Fragment frag;                       
-  };
-
 struct icmphdr
 {
-  u_int8_t type;                
-  u_int8_t code;                
-  u_int16_t checksum;
-  union unanymoous un;
-};
- 
-struct udphdr
-{
-  u_int16_t source;
-  u_int16_t dest;
-  u_int16_t len;
-  u_int16_t check;
+    __u8 type;
+    __u8 code;
+    __sum16 checksum;
+    union
+    {
+        struct
+        {
+            __be16 id;
+            __be16 sequence;
+        } echo;
+        __be32 gateway;
+        struct
+        {
+            __be16 __unused;
+            __be16 mtu;
+        } frag;
+    } un;
 };
 
+struct udphdr
+{
+    u_int16_t source;
+    u_int16_t dest;
+    u_int16_t len;
+    u_int16_t check;
+};
 
 struct GlobalHeader
 {
-    u_int32_t       magicNumber;
+    u_int32_t magicNumber;
     u_int16_t versionMajor;
     u_int16_t versionMinor;
-    u_int32_t                time;
-    u_int32_t      sigfigs;
-    u_int32_t       snaplen;
-    u_int32_t      network;
+    u_int32_t time;
+    u_int32_t sigfigs;
+    u_int32_t snaplen;
+    u_int32_t network;
 };
 
 struct PacketHeader
@@ -86,34 +84,31 @@ struct PacketHeader
     u_int32_t packLen;
 };
 
-
 struct EthernetHeader
 {
-    u_char      destination[6];
-    u_char     source[6];
+    u_char destination[6];
+    u_char source[6];
     u_int16_t ethType;
 };
 
-struct ARP{
+struct ARP
+{
     u_int16_t hardware_type;
     u_int16_t protocol;
     u_char hardware_size;
     u_char protocol_size;
     u_int16_t opcode;
-    u_char  sender_MAC[6];
-    u_char  sender_IP[4];
-    u_char  target_MAC[6];
-    u_char  target_IP[4];
-
+    u_char sender_MAC[6];
+    u_char sender_IP[4];
+    u_char target_MAC[6];
+    u_char target_IP[4];
 };
 
-
-struct sslheader{
+struct sslheader
+{
     u_char record_type;
-    u_char ver1,ver2;
-    u_int16_t len1:8,len2:8;
-    
+    u_char ver1, ver2;
+    u_int16_t len1 : 8, len2 : 8;
 };
-
 
 #endif
